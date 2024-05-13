7724f49dd69d054152ca991be6976f131a14f56d
Arman Z. Papyan <Arman.Z.Papyan@ad.ucom.am>
2024-05-13 17:06:30 +0400

    first commit

---
+++ src/components/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function List({ posts }) {
+    return (
+        <ul>
+            {posts.map((post, i) => (
+                <li key={i}>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+
+export default List;
