a87a475ba8cfb6037c5aba5f0f807080fe690ef4
Arman <papyandeveloper@gmail.com>
2024-05-15 09:21:58 +0400

    edit context

--- src/context/context.js
+++ src/context/context.js
@@ -1,6 +1,47 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
 import {faker} from "@faker-js/faker";
-export const PostContext = createContext({})
+// export const PostContext = createContext({})
+
+export const PostContext = ()=>{
+    const PostContext = createContext({})
+
+    const [posts, setPosts] = useState(() =>
+        Array.from({ length: 30 }, () => createRandomPost())
+    );
+    const [searchQuery, setSearchQuery] = useState("");
+
+    // const PostContext = createContext({})
+
+
+    // Derived state. These are the posts that will actually be displayed
+    const searchedPosts =
+        searchQuery.length > 0
+            ? posts.filter((post) =>
+                `${post.title} ${post.body}`
+                    .toLowerCase()
+                    .includes(searchQuery.toLowerCase())
+            )
+            : posts;
+
+    function handleAddPost(post) {
+        setPosts((posts) => [post, ...posts]);
+    }
+
+    function handleClearPosts() {
+        setPosts([]);
+    }
+
+   return (
+       <PostContext.Provider value={{
+           posts: searchedPosts,
+           onAddPost:handleAddPost,
+           onClearPosts: handleClearPosts,
+           searchQuery,
+           setSearchQuery,
+       } }></PostContext.Provider>
+   )
+
+}
 
 export const  createRandomPost = ()=> {
     return {
